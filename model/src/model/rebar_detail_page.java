8725bd7bba636ca4b287111eb5eb23a2e7497d6f
linker0730 <vic61316@gmail.com>
2023-12-28 22:20:08 +0800

    2023/12/28 mac

--- model/src/model/rebar_detail_page.java
+++ model/src/model/rebar_detail_page.java
@@ -1,6 +1,10 @@
 package model;
+import java.sql.*; // 導入JDBC。
+import java.util.HashMap;
 
 /**
  * 定義植筋、鋼筋。
  */
-public class rebar_detail_page {}
+public class rebar_detail_page {
+    public HashMap<Integer,Float > rebar_datails = new HashMap<>();
+}
