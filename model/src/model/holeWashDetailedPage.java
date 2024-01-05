be0c440266f1dd68bf68d1bc81d032fa51312330
linker0730 <vic61316@gmail.com>
2024-01-05 21:46:33 +0800

    2023/01/05

---
+++ model/src/model/holeWashDetailedPage.java
@@ -0,0 +1,8 @@
+package model;
+
+import org.apache.commons.lang3.tuple.Pair;
+import java.util.HashMap;
+
+public class holeWashDetailedPage extends rebarDetailedPage{
+    public HashMap<Pair<Integer, Integer>, Integer> holeWashDetails;
+}
